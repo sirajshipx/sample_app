da1893cea239d7f7fea400a766d3c9c152919600
siraj <sirajuddin@shipx.in>
2018-10-08 14:27:10 +0530

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-config.assets.initialize_on_precompile = false
+
 module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -44,7 +44,7 @@ module SampleApp
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+config.assets.initialize_on_precompile = false
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
